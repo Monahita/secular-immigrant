411183be43624defcc3251711ea4f65060440ab7
Monahita <monahita@yahoo.de>
2026-07-16 23:17:27 +0200

    Update Featured Articles section

--- components/FeaturedArticles.tsx
+++ components/FeaturedArticles.tsx
@@ -9,7 +9,7 @@ export default function FeaturedArticles() {
     
     <section className="max-w-6xl mx-auto px-6 py-20">
       <h2 className="text-3xl font-bold text-center mb-12 text-red-600">
-  تست FeaturedArticles
+
 </h2>
 
       <div className="grid gap-8 md:grid-cols-2 lg:grid-cols-3">
